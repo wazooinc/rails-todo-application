8390305a8ccf48e2514beab83c91d2fe09cc439b
erikyuzwa <erikyuzwa@gmail.com>
2022-10-01 14:25:28 -0600

    added default route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   resources :todo_items
   devise_for :accounts
+
+  root to: 'todo_items#index'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
